c3caa27d00e1a5fd6a852efa1f000553b453b841
ConstCK <Constantin_CK@outlook.com>
2023-12-28 13:04:14 +0300

    Add doc strings

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from utils import create_html, BanksParser
+from utils import BanksParser
 import os
 
 
 def main():
-    if "html.txt" not in os.listdir():
-        create_html("html.txt")
     parser = BanksParser()
+    if "html.txt" not in os.listdir():
+        parser.create_html("html.txt")
     with open("html.txt", "r") as file:
         src = file.read()
     parser.get_data(src)
     parser.save_to_csv("data.csv")
     parser.save_to_json("data.json")
+    print(parser)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -3,28 +3,35 @@ from bs4 import BeautifulSoup
 import csv
 import json
 
-URL = 'https://krasnodar.bankiros.ru/currency/sochi'
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-}
-
-
-def create_html(f: str):
-    with open(f, "w+") as file:
-        page = requests.get(URL, headers=HEADERS)
-        page.encoding = "utf-8"
-        file.write(page.text)
-
 
 class BanksParser:
+    """Класс-парсер страницы с валютами"""
+
     def __init__(self) -> None:
         self.banks: list = list()
         self.best_usd_buy: str = ""
         self.best_usd_sell: str = ""
         self.best_euro_buy: str = ""
         self.best_euro_sell: str = ""
+        self.url = 'https://krasnodar.bankiros.ru/currency/sochi'
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        }
+
+    def __str__(self) -> str:
+        return f"Список банков с курсами: {self.banks}. {self.best_usd_buy}, {self.best_usd_sell},\
+    {self.best_euro_buy}, {self.best_euro_sell}"
+
+    def create_html(self, f: str):
+        """Метод получения страницы и запись ее в файл"""
+        with open(f, "w+") as file:
+            page = requests.get(self.url, headers=self.headers)
+            page.encoding = "utf-8"
+            file.write(page.text)
 
     def get_data(self, data: str) -> None:
+        """Метод получения списка банков с курсами валют в них"""
         soup = BeautifulSoup(data, "lxml")
         data: list = soup.find_all("div", class_="xxx-tbl-row__grid")
         for element in data:
@@ -46,6 +53,7 @@ class BanksParser:
                 continue
 
     def save_to_csv(self, f: str) -> None:
+        """Метод сохранения данных в csv файл"""
         self.get_best_rate()
         with open(f, "w") as file:
             writer = csv.DictWriter(file, fieldnames=['name', 'usd_buy',
@@ -54,12 +62,13 @@ class BanksParser:
             for element in self.banks:
                 writer.writerow(element)
 
-
     def save_to_json(self, f: str) -> None:
+        """Метод сохранения данных в json файл"""
         with open(f, "w") as file:
             json.dump(self.banks, file, ensure_ascii=False)
 
     def get_best_rate(self) -> None:
+        """Метод получения самых выгодных курсов валют"""
         best_usd_buy = sorted(
             self.banks, key=lambda x: x["usd_buy"], reverse=True)[0]
         best_usd_sell = sorted(self.banks, key=lambda x: x["usd_sell"])[0]
